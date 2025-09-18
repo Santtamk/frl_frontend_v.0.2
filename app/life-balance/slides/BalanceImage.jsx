51b24195c8fac72e57f9a0fda86c8201d1b06e8d
Santtamk <santtamk@gmail.com>
2025-09-18 16:47:02 +0530

    mobile responsive lifebalance

--- app/life-balance/slides/BalanceImage.jsx
+++ app/life-balance/slides/BalanceImage.jsx
@@ -4,15 +4,8 @@ import Lottie from "@lottielab/lottie-player/react";
 
 const BalanceImage = ({ image }) => {
   return (
-    <div className=" w-48 md:w-[20rem] md:h-[20rem] lg:w-[37.5rem] h-full lg:h-[37.5rem] aspect-square overflow-hidden lg:px-0">
-      <div className="sm:scale-[2] scale-[1.8] md: origin-center w-full h-full">
-        <Lottie
-          src={image}
-          autoplay
-          loop
-          style={{ width: "100%", height: "100%" }}
-        />
-      </div>
+    <div className="w-[90vw] sm:w-full sm:max-w-[37.5rem] aspect-square mx-auto">
+      <Lottie src={image} autoplay loop style={{ width: "100%", height: "100%" }} />
     </div>
   );
 };
